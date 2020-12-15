19c0f23aa178a123e2bd49b9f3cc5d23a3aecc8b
Elina <elinabeltran@gmail.com>
2020-12-15 01:20:03 -0300

    Arregle urls de img. Cree las rutas con express.Quedaria agregar las maquetas de los html restantes

--- app.js
+++ app.js
@@ -6,17 +6,24 @@ app.use(express.static('./public'));
 
 
 app.get('/', function(req,res){
-    res.sendFile( path.join(__dirname ,'./views/index.html'))
+    res.sendFile( path.join(__dirname ,'./src/views/index.html'))
 })
 
 app.get('/login', function(req,res){
-    res.sendFile( path.join(__dirname ,'./views/login.html'))
+    res.sendFile( path.join(__dirname ,'./src//views/login.html'))
 })
 
 app.get('/register', function(req,res){
-    res.sendFile( path.join(__dirname ,'./views/register.html'))
+    res.sendFile( path.join(__dirname ,'./src//views/register.html'))
+})
+app.get('/productCart', function(req,res){
+    res.sendFile( path.join(__dirname ,'./src//views/productCart.html'))
+})
+app.get('/productDetail', function(req,res){
+    res.sendFile( path.join(__dirname ,'./src//views/productDetail.html'))
 })
 
 app.listen (3000, function(){
     console.log('El servidor está corriendo en puerto 3000')
 })
+
