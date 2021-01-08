8a4ce868002eb107144eee8e4fbbb7ce6dcb1407
Nath <nathywar2000@gmail.com>
2021-01-07 22:20:45 -0300

    se agrego sistema de rutas, controllers y plantillas

--- app.js
+++ app.js
@@ -1,49 +1,16 @@
-const express= require ('express');
+const express = require('express');
 const app = express();
 const path= require('path');
 
-app.use(express.static('./public'));
+var indexRouter = require('./src/routes/index');
 
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "src/views"));
 
-app.get('/', function(req,res){
-    res.sendFile( path.join(__dirname ,'./src/views/index.html'))
-});
+app.use(express.static(path.join(__dirname, './public')));
 
-app.get('/login', function(req,res){
-    res.sendFile( path.join(__dirname ,'./src/views/login.html'))
-});
+app.use('/', indexRouter);
 
-app.get('/registro', function(req,res){
-    res.sendFile( path.join(__dirname ,'./src/views/registro.html'))
-});
-
-app.get('/productCart', function(req,res){
-    res.sendFile( path.join(__dirname ,'./src/views/productCart.html'))
-});
-
-app.get('/productDetail', function(req,res){
-    res.sendFile( path.join(__dirname ,'./src/views/productDetail.html'))
-});
-
-app.get('/productos/:idProducto', function(req,res){
-    res.send( path.join("Bienvenido al detalle de producto "+ req.params.idProducto))
-});
-
-app.get('/productos/:idProducto/comentarios/:idComentario?', function(req,res){
-
-    if( req.params.idComentario == undefined){
-    res.send( path.join("Bienvenido los comentarios de producto "+ req.params.idProducto  ))
-
-} else{
-    res.send( path.join("Bienvenido los comentarios de producto "+ req.params.idProducto +" y estas enfocado en le comentario " +req.params.idComentario ))
-
-}
-
-
-});
-
-
-app.listen (3000, function(){
-    console.log('El servidor está corriendo en puerto 3000')
+app.listen(3000, function(){
+    console.log('Esta corriendo sobre el puerto 3000')
 })
-
