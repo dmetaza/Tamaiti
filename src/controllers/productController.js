5b31696073acd3971b3b76d1f18c67ecabc8670b
Elina <elinabeltran@gmail.com>
2021-02-07 14:47:00 -0300

    incorpore id a create product - incorpore carga de img

--- src/controllers/productController.js
+++ src/controllers/productController.js
@@ -5,44 +5,54 @@ const path = require('path');
 let productos = fs.readFileSync(path.join(__dirname, '../data/products.json'), 'utf8');
 productos = JSON.parse(productos);
 
-module.exports = {
+module.exports = {  
     index: function(req, res){
         res.render('pages/products', {productos:productos});
     },
     cart: function(req, res){
         res.render('pages/cart');
     },
+    boardCreate: function(req, res){
+         res.render('pages/productosCreate');
+    },  
+
+    create: function(req, res){    
+
+        let id = productos.length+1
+        
+        productos.push({
+            id:id,
+            name:req.body.name,
+            image: req.files[0].filename,
+            description:req.body.description,
+            price:req.body.price,
+            discount:req.body.discount,
+            age:req.body.age,
+            category:req.body.category,
+        })
+
+        
+        fs.writeFileSync(path.join(__dirname, '../data/products.json'), JSON.stringify(productos));
+        console.log(req.files)
+        res.redirect('/products')
+    },
     detail: function(req, res){
         let pedidoProducto = req.params.id;
 
         for (let i = 0; i < productos.length; i++) {
+           
             if (pedidoProducto == productos[i].id) {
                  res.render('pages/details', {
                  elProducto: productos[i],
                  productos: productos
+
                 }) 
             }
-         }
-       return res.send (pedidoProducto)
-    },
+ 
+        }
+           return res.send (pedidoProducto)
+    }
 
-    boardCreate: function(req, res){
-        res.render('pages/productosCreate');
-    },
-
-    create: function(req, res){    
-        productos.push({
-            name:req.body.name,
-            description:req.body.description,
-            price:req.body.price,
-            age:req.body.age,
-            category:req.body.category,
-
-        })
-        fs.writeFileSync(path.join(__dirname, '../data/products.json'), JSON.stringify(productos));
-        console.log(req.body)
-        res.redirect('/products')
-    },
 
 
 
--- src/routes/productRoutes.js
+++ src/routes/productRoutes.js
@@ -1,22 +1,34 @@
 const express = require('express');
-
+const router = express.Router();
+const multer = require('multer');
+const path = require('path');
 const productController = require('../controllers/productController');
 
-const router = express.Router();
+
+
+
+var storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+      cb(null, path.join(__dirname, '../../public/uploads/img_products'))
+    },
+    filename: function (req, file, cb) {
+      cb(null, req.body.name + Date.now() + path.extname(file.originalname))
+    }
+  })
+   
+var upload = multer({ storage: storage })
+
 
 router.get('/', productController.index);
 
 
 router.get('/create', productController.boardCreate);
-router.post('/create', productController.create);
+router.post('/create',upload.any(), productController.create);
 //router.put('/create', productController.edit);
 //router.delete('/create', productController.delete);
-
 //router.get('/productBoard', productController.board);
 
-
 router.get('/:id', productController.detail);
-
 router.get('/cart', productController.cart);
 
 
