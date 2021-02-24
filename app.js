d9944762ae0908a239297edb110d9d5d6e14d41a
Lucia Roman <lucia.roman@riseinteractive.com>
2021-02-24 20:56:47 -0300

    trabajando sobre controllers

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const app = express();
 const path = require('path');
 const port = process.env.port || 3000;
 const methodOverride= require('method-override');
+const session = require ('express-session');
 
 var indexRouter = require('./src/routes/index');
 
@@ -14,7 +15,7 @@ app.use(express.static(path.join(__dirname, './public')));
 app.use(express.urlencoded({extended: false}));
 app.use(express.json());
 app.use(methodOverride('_method'));
-
+app.use(session ({secret:'Secreto!'}));
 
 app.use('/', indexRouter);
 
